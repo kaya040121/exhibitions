1b8fb7e2a03f001d5b85de01acad2c639917e3ad
kaya040121 <kaya040121@gmail.com>
2021-08-19 10:57:59 +0800

    Update 2021-08-19T02:57:51.253Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,16 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+}); // swiper
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
