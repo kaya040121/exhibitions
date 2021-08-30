136d9ce3607f10bb6cb3f540c53f8eccf3f4620d
kaya040121 <kaya040121@gmail.com>
2021-08-30 18:58:40 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,15 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
+});
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  }
 });
